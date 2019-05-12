83721d15d50df2610faf4a9f747ea3fb0e0fccdf
Paula Tulis <tulis.paula@gmail.com>
2019-05-12 12:06:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Paula"
+ end
+
+ get '/hometown' do
+   "My hometown is Atlanta, GA"
+end
+
+get '/favorite-song' do
+  "My favorite song is I don't have a favorite song right now" 
+end
+
 end
